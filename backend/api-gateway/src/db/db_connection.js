d4a7aa60dd86faa9c82298cc87b78d2202a4ec04
VarunShukla07 <varun.shuklaoffical@gmail.com>
2026-09-20 22:20:08 +0530

    Added Logs

--- backend/api-gateway/src/db/db_connection.js
+++ backend/api-gateway/src/db/db_connection.js
@@ -8,6 +8,16 @@ const pool = new Pool({
   }
 });
 
+console.log('[db] node version:', process.version);
+console.log('[db] effective ssl config:', JSON.stringify(pool.options.ssl));
+console.log('[db] DATABASE_URL:', (() => {
+  try{
+    return new URL(process.env.DATABASE_URL).host;
+  }catch{
+    return 'unparasable';
+  }
+})());
+
 pool.on('error', (err) => {
   console.error('Postgres error', err);
 });
